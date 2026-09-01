5ab353dec6f51cbb5599a50bd2c564379fdd3cab
Coach Clint <clint.l.stevens@gmail.com>
2026-08-31 21:56:48 -0600

    Clean SuperUser route imports

--- app/superuser/page.tsx
+++ app/superuser/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React from 'react';
-import SuperUserWorkspace from '@/components/hubs/superuser/SuperUserWorkspace';
 import ProjectCommand from '@/components/hubs/superuser/ProjectCommand';
 
 export default function SuperUserPage(){
