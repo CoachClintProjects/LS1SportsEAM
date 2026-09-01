e0c46b36830b7a45da8a80567066c25359a85399
Coach Clint <clint.l.stevens@gmail.com>
2026-08-31 21:56:43 -0600

    Use single SuperUser workspace surface

--- app/superuser/page.tsx
+++ app/superuser/page.tsx
@@ -1,18 +1,9 @@
-﻿'use client';
-
-// =====================================================
-// LS1Sports SuperUser Route
-// =====================================================
+'use client';
 
 import React from 'react';
 import SuperUserWorkspace from '@/components/hubs/superuser/SuperUserWorkspace';
 import ProjectCommand from '@/components/hubs/superuser/ProjectCommand';
 
 export default function SuperUserPage(){
-  return <>
-    <ProjectCommand />
-    <div className="mt-6">
-      <SuperUserWorkspace />
-    </div>
-  </>;
+  return <ProjectCommand />;
 }
