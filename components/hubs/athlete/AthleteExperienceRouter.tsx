53541f330dc171ef1103c5101d4e98665f09200c
Coach Clint <clint.l.stevens@gmail.com>
2026-09-09 15:37:03 -0600

    Use authenticated Foundation Athlete hub

--- components/hubs/athlete/AthleteExperienceRouter.tsx
+++ components/hubs/athlete/AthleteExperienceRouter.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import YoungAthleteAdventure from './YoungAthleteAdventure';
+import FoundationAthleteHub from './FoundationAthleteHub';
 import AthleteCapitalHub from './AthleteCapitalHub';
 
 export default function AthleteExperienceRouter() {
   const params = useSearchParams();
   const age = params.get('age') || '5-8';
-  return age === '5-8' ? <YoungAthleteAdventure /> : <AthleteCapitalHub />;
+  return age === '5-8' ? <FoundationAthleteHub /> : <AthleteCapitalHub />;
 }
