2e1a499ee5a5e0d4f837f8e28cdbc83b1f17602f
Coach Clint <clint.l.stevens@gmail.com>
2026-09-09 15:35:58 -0600

    Use typed Athlete capital hub

--- components/hubs/athlete/AthleteExperienceRouter.tsx
+++ components/hubs/athlete/AthleteExperienceRouter.tsx
@@ -2,10 +2,10 @@
 
 import { useSearchParams } from 'next/navigation';
 import YoungAthleteAdventure from './YoungAthleteAdventure';
-import AthleteCapitalAssetWorkspace from './AthleteCapitalAssetWorkspace';
+import AthleteCapitalHub from './AthleteCapitalHub';
 
 export default function AthleteExperienceRouter() {
   const params = useSearchParams();
   const age = params.get('age') || '5-8';
-  return age === '5-8' ? <YoungAthleteAdventure /> : <AthleteCapitalAssetWorkspace />;
+  return age === '5-8' ? <YoungAthleteAdventure /> : <AthleteCapitalHub />;
 }
