e438ceb02d8f613aa386b5d2394a5d622f113b45
Coach Clint <clint.l.stevens@gmail.com>
2026-09-11 22:11:21 -0600

    Wire Coach decision intelligence workspace

---
+++ app/coach/analytics/page.tsx
@@ -0,0 +1,3 @@
+import CoachDecisionIntelligence from '@/components/hubs/coach/CoachDecisionIntelligence';
+export const dynamic='force-dynamic';
+export default function CoachAnalyticsPage(){return <CoachDecisionIntelligence/>}
