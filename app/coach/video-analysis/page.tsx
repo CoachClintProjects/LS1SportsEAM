28ee9d42d96770b812470f91c8d1a74c0d41f47b
Coach Clint <clint.l.stevens@gmail.com>
2026-09-14 13:58:55 -0600

    Add Coach stroke video analysis page

---
+++ app/coach/video-analysis/page.tsx
@@ -0,0 +1,5 @@
+import CoachKnowledgeWorkspace from '@/components/hubs/coach/CoachKnowledgeWorkspace';
+
+export default function CoachVideoAnalysisPage() {
+  return <CoachKnowledgeWorkspace mode="video-analysis" />;
+}
