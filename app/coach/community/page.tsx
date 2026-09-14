c7f059d378a3faaca41a842e49e9232e1339b944
Coach Clint <clint.l.stevens@gmail.com>
2026-09-14 13:58:44 -0600

    Add Coach community page

---
+++ app/coach/community/page.tsx
@@ -0,0 +1,5 @@
+import CoachKnowledgeWorkspace from '@/components/hubs/coach/CoachKnowledgeWorkspace';
+
+export default function CoachCommunityPage() {
+  return <CoachKnowledgeWorkspace mode="community" />;
+}
