7a7d9b72eba05061effb8f80388174de5e3d5f7f
Coach Clint <clint.l.stevens@gmail.com>
2026-09-14 13:58:51 -0600

    Add Coach drill library page

---
+++ app/coach/library/page.tsx
@@ -0,0 +1,5 @@
+import CoachKnowledgeWorkspace from '@/components/hubs/coach/CoachKnowledgeWorkspace';
+
+export default function CoachLibraryPage() {
+  return <CoachKnowledgeWorkspace mode="library" />;
+}
